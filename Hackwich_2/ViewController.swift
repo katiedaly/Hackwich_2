39312a60e12e5fd5db18dfefdaa86cfc3d1d9552
katiedaly <dalyk@hawaii.edu>
2021-01-26 11:36:00 -1000

    Completed parts 1-8

--- Hackwich_2/ViewController.swift
+++ Hackwich_2/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
-
+// type of object - class
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // {} - curly
+        // any code that you put into viewDidLoad block is your initial setting for the application
     }
 
+    
+// Part 7 - We are going to create our first IB Action
 
+    @IBAction func pressMeButtonPressed(_ sender: Any)
+    {
+    // This is the code block for the pressMeButtonPressed function
+        
+    // Part 7, Number 5
+            print("Hello World!")
 }
 
+// Part 8 - Add another button
+
+
+    @IBAction func clickHereButtonPressed(_ sender: Any) {
+    
+print("I got this!")
+}
+}
