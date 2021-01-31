fe16c6501f5c00f95b2c27aad3415fdd2a8387e2
katiedaly <dalyk@hawaii.edu>
2021-01-31 01:49:04 -1000

    Completed Parts 1-7

--- Hackwich_2/SecondViewController.swift
+++ Hackwich_2/SecondViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class secondViewController: UIViewController {
 
+    
+// Part 7.1, Create an IBOutlet
+    @IBOutlet weak var firstLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +25,12 @@ class secondViewController: UIViewController {
     // code block
     // When button is pressed, change the background color to black
         self.view.backgroundColor = UIColor.black
+        
+    // 7.3 - Set the text property of the label to a string
+        self.firstLabel.text = "I did it!"
+    // 7.4 - Set the textColor property of the label
+        firstLabel.textColor = UIColor(red: 0.05, green: 1.0, blue: 1.0, alpha: 1.0)
     
-    }
+        
+}
 }
